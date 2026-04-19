f931006f9943b207ebbed1ffff52f2b8cf3b16cc
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-19 22:47:59 +0600

    Init java project: src/main/java/com/example/ecssingrb3/Ecssingrb3Application.java

---
+++ src/main/java/com/example/ecssingrb3/Ecssingrb3Application.java
@@ -0,0 +1,13 @@
+package com.example.ecssingrb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecssingrb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecssingrb3Application.class, args);
+	}
+
+}
